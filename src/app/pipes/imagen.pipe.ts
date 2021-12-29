76bb7790ae7afe0944ffde603f658d90cefe1ae6
zentaker <apalomino12@icloud.com>
2021-12-28 21:14:46 -0500

    update

--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -1,12 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url;
 
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(img: string, tipos:'usuarios'|'asesores' ): string {
-    return 'hola mundo';
+  transform(img: string, tipo:'usuarios'|'asesores' ): string {
+    if(!img) {
+      return `${base_url}/upload/usuarios/no-img-user`;
+
+    }else if (img?.includes('http')) {
+      return img;
+    }else if(img){
+      return `${base_url}/upload/${tipo}/${img}`;
+    }else {
+      return `${base_url}/upload/usuarios/no-img-user`;
+
+    }
   }
 
 }
